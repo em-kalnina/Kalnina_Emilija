78fd9352d8074e2f44c106ea91c8858b04d1dc93
em-kalnina <emilija.kaa@inbox.lv>
2025-03-24 15:37:20 +0200

    edited

---
+++ OOP.py
@@ -0,0 +1,40 @@
+# # # class Persona:
+# # #     def __init__(self, vards, vecums):
+# # #         self.vards = vards
+# # #         self.vecums = vecums
+# # #     def drukat(self):
+# # #         print(self.vards, self.vecums)
+
+# # # persona = Persona("Emīlija", 18)
+# # # persona.drukat
+
+# # class Persona: 
+# #     def __init__(self, vards, vecums):
+# #         self.vards = vards
+# #         self.vecums = vecums
+# #     def drukat(self):
+# #         print(self.vards, self.vecums)
+
+# # class Students(Persona): 
+# #     def __init__(self, vards, vecums, studiju_gads):
+# #         super().__init__(vards, vecums) 
+# #         self.studiju_gads = studiju_gads 
+# #     def drukat(self): 
+# #         super().drukat() 
+# #         print(self.studiju_gads)
+
+# # persona = Persona("Pēteris", 20) 
+# # persona.drukat() 
+# # students = Students("Linda", 19, 2) 
+# # students.drukat() 
+
+# class A1:
+# pass
+# class A2:
+# pass
+# class B(A1, A2): # daudzkāršā mantošana
+# pass
+# a1 = A1()
+# a2 = A2()
+# b = B()
+# c = C()
